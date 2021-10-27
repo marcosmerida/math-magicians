923823e05deadbcbc5465719912c0bcc22facfa2
marcosmerida <marcosmerida@gmail.com>
2021-10-26 18:17:59 -0600

    Change app to be class-based

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
 import Calculator from './component/Calculator';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return <Calculator />;
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
