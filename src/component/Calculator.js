91c5af6d1ce68ddcb5b2cefbdef07ee0d3f2f0c3
marcosmerida <marcosmerida@gmail.com>
2021-10-26 18:02:02 -0600

    Make calculator design bigger

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -10,69 +10,69 @@ function Calculator() {
         </div>
       </div>
       <div className="row h-100 r1">
-        <div className="col-3 border border-white" id="button1">
+        <div className="col-3 border border-white h-100" id="button1">
           AC
         </div>
-        <div className="col-3 border border-white" id="button2">
+        <div className="col-3 border border-white h-100" id="button2">
           +/-
         </div>
-        <div className="col-3 border border-white" id="button3">
+        <div className="col-3 border border-white h-100" id="button3">
           %
         </div>
-        <div className="col-3 border border-white bg-warning" id="button4">
+        <div className="col-3 border border-white bg-warning h-100" id="button4">
           ÷
         </div>
       </div>
-      <div className="row h-100 r1">
-        <div className="col-3 border border-white" id="button5">
+      <div className="row r1 h-100">
+        <div className="col-3 border border-white h-100" id="button5">
           7
         </div>
-        <div className="col-3 border border-white" id="button6">
+        <div className="col-3 border border-white h-100" id="button6">
           8
         </div>
-        <div className="col-3 border border-white" id="button7">
+        <div className="col-3 border border-white h-100" id="button7">
           9
         </div>
-        <div className="col-3 border border-white bg-warning" id="button8">
+        <div className="col-3 border border-white bg-warning h-100" id="button8">
           x
         </div>
       </div>
       <div className="row h-100 r1">
-        <div className="col-3 border border-white" id="button9">
+        <div className="col-3 border border-white h-100" id="button9">
           4
         </div>
-        <div className="col-3 border border-white" id="button10">
+        <div className="col-3 border border-white h-100" id="button10">
           5
         </div>
-        <div className="col-3 border border-white" id="button11">
+        <div className="col-3 border border-white h-100" id="button11">
           6
         </div>
-        <div className="col-3 border border-white bg-warning" id="button12">
+        <div className="col-3 border border-white bg-warning h-100" id="button12">
           -
         </div>
       </div>
       <div className="row h-100 r1">
-        <div className="col-3 border border-white" id="button13">
+        <div className="col-3 border border-white h-100" id="button13">
           1
         </div>
-        <div className="col-3 border border-white" id="button14">
+        <div className="col-3 border border-white h-100" id="button14">
           2
         </div>
-        <div className="col-3 border border-white" id="button15">
+        <div className="col-3 border border-white h-100" id="button15">
           3
         </div>
-        <div className="col-3 border border-white bg-warning" id="button16">
+        <div className="col-3 border border-white bg-warning h-100" id="button16">
           +
         </div>
       </div>
       <div className="row h-100 r1">
-        <div className="col-6 border border-white" id="button17">
+        <div className="col-6 border border-white h-100" id="button17">
           0
         </div>
-        <div className="col-3 border border-white" id="button18">
+        <div className="col-3 border border-white h-100" id="button18">
           .
         </div>
-        <div className="col-3 border border-white bg-warning" id="button19">
+        <div className="col-3 border border-white bg-warning h-100" id="button19">
           =
         </div>
       </div>
