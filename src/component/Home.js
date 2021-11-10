6e6a568cc68b877c0409d8034b876b64f3bacb60
marcosmerida <marcosmerida@gmail.com>
2021-11-10 15:54:43 -0600

    Fix linters errors

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ function Homefun() {
       <h2>Welcome to our page</h2>
       <p className="mt-3 h5">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
@@ -17,7 +17,7 @@ function Homefun() {
       </p>
       <p className="mt-3 h5">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
